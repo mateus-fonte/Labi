ed00026141bd3d30fb6d3770c3e5a4603f0da7a2
Mateus da Fonte <mateus.fonte@ua.pt>
2022-03-18 20:15:31 +0000

    adicao das pastas

---
+++ Exercicios/Ex11.py
@@ -0,0 +1,9 @@
+import hashlib, sys
+h = hashlib.sha1()
+with open (sys.stdin.fileno(),"rb") as f:
+    while True:
+        buffer = f.read(1024)
+        if len(buffer) == 0:
+            break
+        h.update(buffer)
+print(h)
