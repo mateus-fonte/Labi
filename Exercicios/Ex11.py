8c16a725f4ba84fef22a096503471d91f623c2ec
Mateus da Fonte <mateus.fonte@ua.pt>
2022-03-18 20:46:57 +0000

    Alteracoes

--- Exercicios/Ex11.py
+++ Exercicios/Ex11.py
@@ -1,6 +1,8 @@
-import hashlib, sys
+import hashlib
+import sys
+
 h = hashlib.sha1()
-with open (sys.stdin.fileno(),"rb") as f:
+with open(sys.stdin.fileno(), "rb") as f:
     while True:
         buffer = f.read(1024)
         if len(buffer) == 0:
